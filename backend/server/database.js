37332c91cf72edef1306c8772c03df79b8a46a2e
Priyanshu <priyanshukamde22@gmail.com>
2024-10-26 02:04:27 +0530

    commit1

---
+++ backend/server/database.js
@@ -0,0 +1,30 @@
+const {Client} = require('pg')
+
+const client = new Client({
+
+host: "localhost",
+
+user: "postgres",
+
+port: 5432,
+
+password: "Priyanshu@22",
+
+database: "postgres"
+
+})
+
+client.connect();
+
+client.query('Select * from users',(err, res)=>{ 
+    if(!err){
+
+console.log(res.rows);
+
+}else {
+
+console.log(err.message); }
+
+client.end;
+
+})
